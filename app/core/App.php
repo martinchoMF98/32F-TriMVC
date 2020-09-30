eac9c426b8c52fc5e8c56c9b5683bcfd271862e4
Martin Aguado <themartinex98@gmail.com>
2020-09-30 09:11:07 +0200

    app atributs

--- app/core/App.php
+++ app/core/App.php
@@ -1,5 +1,8 @@
 <?php
 class App {
+    protected $contoller = 'home';
+    protected $method = 'index';
+    protected $params = [];
     public function __construct()
     {
         echo "App is running succesfully";
